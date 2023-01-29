3b74b29a3907decf6bb88681370f1a6f177e3084
caleb jephuneh <calebjephunneh@gmail.com>
2023-01-30 00:58:10 +0300

    dashboard

---
+++ pages/dashboard.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+const DashboardPage = () => {
+    return (
+      <>
+          <Head>
+            <title> Dashboard </title>
+          </Head>
+          <div className="flex py-2 container mx-auto">
+            <div className="text-gray-600 px-12 py-24 mt-24 overflow-y-hidden mx-auto">
+              <h2 className="text-2xl font-semibold">You are logged in!</h2>
+            </div>
+          </div>
+      </>
+    );
+  };
+  
+  export default DashboardPage;
